06fc7e7e4e057eae9c514da522e21e1288a4c161
torone89 <torone89>
2022-07-04 14:15:00 +0200

    Genero 5 numeri casuali e li stampo nel DOM

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@ console.log("JS")
 // Visualizzare in pagina 5 numeri casuali  diversi tra loro. Da lì parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite i prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+// Recupero dove stampare i numeri da ricordare a mente nel DOM
+
+const numeri = document.getElementById('numerimente')
+console.log(numeri)
+
+
+// Mi creo una funzione per generare 5 numeri casuali da 1 a 100 e li aggiungo
+function getRandomnumber(min, max) {
+
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+let numerimente = [];
+for (let i = 1; i <= 5; i++) {
+
+    numerimente.push(getRandomnumber(1, 100))
+    console.log(getRandomnumber(1, 100))
+    numeri.innerText = numerimente + " "
+}
