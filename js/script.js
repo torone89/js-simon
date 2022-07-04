3882fbb5677cf5f685f425a8c4aefc5075e1f976
torone89 <torone89>
2022-07-04 13:35:53 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// VERIFICA COLLEGAMENTO JAVA
+console.log("JS")
+
+
+// Visualizzare in pagina 5 numeri casuali  diversi tra loro. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite i prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
