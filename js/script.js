2a542899a6e6a0fa6085c1d67cc1c20907af7a0f
torone89 <torone89>
2022-07-04 17:37:30 +0200

    Creoo array per la prompt

--- js/script.js
+++ js/script.js
@@ -28,17 +28,23 @@ const indicenumeri = 5
 
 for (let i = 1; i <= indicenumeri; i++) {
 
-    // Stabilisco se posso inserire numeri doppi
-    let numericasuali = 0
 
-    do {
-        numericasuali = getRandomnumber(1, 100);
-    } while (numerimente.includes(numericasuali));
+    // Controllo 
+    if (numerimente.includes(getRandomnumber(1, 100))) { i-- }
+    else { numerimente.push(getRandomnumber(1, 100)) }
 
-    numerimente.push(getRandomnumber(1, 100))
     console.log(numerimente)
-    numeri.innerText = numerimente + " "
+    numeri.innerText = numerimente
+
 }
+
+// // SOLUZIONE WHILE
+// while (numerimente.length <= 5) {
+//     let randomNumber = getRandomnumber(1, 100);
+//     if (!numerimente.includes(randomNumber)) {
+//         numerimente.push(randomNumber);
+//     }
+// }
 
 // Mi creo un ContDown che parte da 30 secondi e decresci di un secondo fino allo 0
 let secondi = 5
@@ -49,20 +55,27 @@ const contoAllaRovescia = setInterval(function () {
         // Aggiungo la classe display none
         numeri.classList.add('display');
 
-
-        for (let i = 1; i <= 5; i++) {
-
-            let indovina = parseInt(prompt(alert("Inserisci il numero  che ti ricordi")))
-
-            console.log(indovina[i])
-
-            if (indovina === numerimente) { risulatofinale = +1 }
-
-
-            let risulatofinale = indovina
-            console.log("Hai indovinato" + risulatofinale + "numeri")
-        }
-
     }
     console.log(contoAllaRovescia)
 }, 1000)
+
+
+// Mi Creo un array per chiedere un utente un numero 
+
+const indovina = [];
+
+// Imposto un prompt che chieda all'utente i numeri visti precedentemente a 0.2s
+const comandoprompt = setTimeout(function () {
+
+    // Creo un ciclo in base all'indice dei numeri che precedentemnte ho viusalizzato
+    for (let i = 1; i <= indicenumeri; i++) {
+        let indovinaNumero
+        do {
+            indovinaNumero = parseInt(prompt("Inserisci i numeri che ti ricordi di quelli che hai visualizzato"))
+        } while (isNaN(indovinaNumero)) // Verifica che sia un valore "numero" e non stringa
+
+        // Inserisco il numero nell'array Indovina
+        indovina.push(indovinaNumero);
+    }
+    console.log("I numeri che ho scelto: " + indovina);
+}, secondi * 1000 + 200);
